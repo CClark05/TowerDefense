ad7a96726f8fd2112c3363b0cd5020a20d6caff4
CClark05 <chclark543@gmail.com>
2025-11-25 17:51:17 -0600

    FIRST DEMO

--- Assets/Scripts/UI/Inventory/InventoryUI.cs
+++ Assets/Scripts/UI/Inventory/InventoryUI.cs
@@ -70,11 +70,13 @@ public class InventoryUI : Singleton<InventoryUI>, IUsesCards
     private int test = 0;
     private void Update()
     {
+        /**
         if (Input.GetKeyDown(KeyCode.Space))
         {
             AddCard(testingData[test].CreateInstance());
             test++;
         }
+        */
     }
     
     public void AddCard(SkillInstance skillInstance)
